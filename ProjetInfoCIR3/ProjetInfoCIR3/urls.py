c86e45fe3c2807f9c639f74a5c3b4d06344188ac
KhalidBensaber <112802107+KhalidBensaber@users.noreply.github.com>
2025-01-15 12:06:59 +0100

    tt

--- ProjetInfoCIR3/ProjetInfoCIR3/urls.py
+++ ProjetInfoCIR3/ProjetInfoCIR3/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('logout/', home_views.custom_logout, name='logout'), # Directs to the Django logout view
     path('home/', include('home.urls')),
     path('register/', include('register.urls')),
+    path('api/', (include('home.api.urls'))),
 ]
