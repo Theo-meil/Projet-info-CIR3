27f1b633184c4008ead7629e775b4499f9fa7682
fabiendevaux <112878428+fabiendevaux@users.noreply.github.com>
2025-01-16 10:48:09 +0100

    Update Fonction_db.py

--- ProjetInfoCIR3/ProjetInfoCIR3/BDD/Fonction_db.py
+++ ProjetInfoCIR3/ProjetInfoCIR3/BDD/Fonction_db.py
@@ -50,36 +50,36 @@ def Get_Utilisateur_qrcode(id):
 # Setteur de la tabble utilisateur
 
 def Set_utilisateur(Nom,Prenom,Pseudo,Mot_de_passe,Email,Status,Winstreak,Win,Lose):
-    return db.utilisateurs.update_One({"_id" : id} ,{"$set" : {"nom": Nom,"prenom" : Prenom , "pseudo": Pseudo, "mdp": Mot_de_passe, "email": Email, "status" : Status, "winstreak": Winstreak, "win" : Win , "lose" : Lose} })
+    return db.utilisateurs.update_one({"_id" : id} ,{"$set" : {"nom": Nom,"prenom" : Prenom , "pseudo": Pseudo, "mdp": Mot_de_passe, "email": Email, "status" : Status, "winstreak": Winstreak, "win" : Win , "lose" : Lose} })
 def Set_Utilisateur_Nom(id,Nom): 
-    return db.utilisateurs.update_One({"_id" : id} ,{"$set" :  {"nom" : Nom} })
+    return db.utilisateurs.update_one({"_id" : id} ,{"$set" :  {"nom" : Nom} })
 def Set_Utilisateur_Prenom(id,Prenom): 
-    return db.utilisateurs.update_One({"_id" : id} ,{"$set" :  {"prenom" : Prenom} })
+    return db.utilisateurs.update_one({"_id" : id} ,{"$set" :  {"prenom" : Prenom} })
 def Set_Utilisateur_Pseudos(id,Pseudo): 
-    return db.utilisateurs.update_One({"_id" : id} ,{"$set" :  {"pseudo": Pseudo} })
+    return db.utilisateurs.update_one({"_id" : id} ,{"$set" :  {"pseudo": Pseudo} })
 def Set_Utilisateur_Mot_de_passe(id,Mot_de_passe): 
-    return db.utilisateurs.update_One({"_id" : id} , {"$set" : {"mdp" : Mot_de_passe} })
+    return db.utilisateurs.update_one({"_id" : id} , {"$set" : {"mdp" : Mot_de_passe} })
 def Set_Utilisateur_email(id,Email): 
-    return db.utilisateurs.update_One({"_id" : id} ,{"$set" :  {"email" : Email} })
+    return db.utilisateurs.update_one({"_id" : id} ,{"$set" :  {"email" : Email} })
 def Set_Utilisateur_cb(id,Cb): 
-    return db.utilisateurs.update_One({"_id" : id}, {"$set" :  {"cb" : Cb} })
+    return db.utilisateurs.update_one({"_id" : id}, {"$set" :  {"cb" : Cb} })
 def Set_Utilisateur_Status(id,Status): 
-    return db.utilisateurs.update_One({"_id" : id} ,{"$set" : {"status" : Status} })
+    return db.utilisateurs.update_one({"_id" : id} ,{"$set" : {"status" : Status} })
 def Set_Utilisateur_Winstreak(id,Winstreak): 
-    return db.utilisateurs.update_One({"_id" : id} ,{"$set" : {"winstreak" : Winstreak} })
+    return db.utilisateurs.update_one({"_id" : id} ,{"$set" : {"winstreak" : Winstreak} })
 def Set_Utilisateur_Win(id,Win): 
-    return db.utilisateurs.update_One({"_id" : id},{"$set" : {"win" : Win} })
+    return db.utilisateurs.update_one({"_id" : id},{"$set" : {"win" : Win} })
 def Set_Utilisateur_lose(id,Lose): 
-    return db.utilisateurs.update_One({"_id" : id} ,{"$set" :  {"lose" : Lose} })
+    return db.utilisateurs.update_one({"_id" : id} ,{"$set" :  {"lose" : Lose} })
 def Set_Utilisateur_lose(id,Qrcode): 
-    return db.utilisateurs.update_One({"_id" : id} ,{"$set" :  {"qrcode" : Qrcode} })
+    return db.utilisateurs.update_one({"_id" : id} ,{"$set" :  {"qrcode" : Qrcode} })
 
 
 
 
 # Equipe
 def Add_Equipe(Nom,Tab_joueur,Manageur,Jeux):
-    return db.Equipe.insert_One({"nom": Nom, "tab_joueur" : Tab_joueur, "manageur" : Manageur, "jeux" : Jeux})
+    return db.Equipe.insert_one({"nom": Nom, "tab_joueur" : Tab_joueur, "manageur" : Manageur, "jeux" : Jeux})
 
 def Sup_Equipe(id):
     return db.Equipe.delete_one({"_id" : id})
@@ -102,16 +102,16 @@ def Get_Equipe_jeux(id):
 
 #setteur de Equipes
 def Set_Equipes(Nom,Tab_joueur,Manageur,Jeux):
-    return db.Equipes.update_One({"_id" : id} ,{"$set" : {"nom": Nom, "tab_joueur" : Tab_joueur, "manageur" : Manageur, "jeux" : Jeux}})
+    return db.Equipes.update_one({"_id" : id} ,{"$set" : {"nom": Nom, "tab_joueur" : Tab_joueur, "manageur" : Manageur, "jeux" : Jeux}})
 def Set_Equipes_Nom(id,Nom): 
-    return db.Equipes.update_One({"_id" : id} ,{"$set" :  {"nom" : Nom} })
+    return db.Equipes.update_one({"_id" : id} ,{"$set" :  {"nom" : Nom} })
 def Set_Equipes_tab_joueur(id,Tab_joueurs): 
-    return db.Equipes.update_One({"_id" : id} ,{"$set" :  {"tab_joueur" : Tab_joueurs} })
+    return db.Equipes.update_one({"_id" : id} ,{"$set" :  {"tab_joueur" : Tab_joueurs} })
 def Set_Equipes_Manageur(id,Manageur): 
-    return db.Equipes.update_One({"_id" : id} ,{"$set" :  {"manageur" : Manageur} })
+    return db.Equipes.update_one({"_id" : id} ,{"$set" :  {"manageur" : Manageur} })
 
 def Set_Equipes_jeux(id,Jeux): 
-    return db.Equipes.update_One({"_id" : id} ,{"$set" :  {"jeux" : Jeux} })
+    return db.Equipes.update_one({"_id" : id} ,{"$set" :  {"jeux" : Jeux} })
 
 
 #table Event ( tournoi)
@@ -152,23 +152,23 @@ def Get_Event_status(id):
 def Set_Event(Nom,Date_debut,Date_fin,Places_max,Places_libres,Cash_price,Status): 
     return db.event.update_One({"_id" : id} ,{"$set" :   {"nom" : Nom , "date_deput": Date_debut, "date_fin" : Date_fin, "places_max" : Places_max, "places_libres" : Places_libres, "cash_price" : Cash_price, "status" : Status}})
 def Set_Event_nom(id,Nom): 
-    return db.event.update_One({"_id" : id} ,{"$set" :  {"nom" : Nom} })
+    return db.event.update_one({"_id" : id} ,{"$set" :  {"nom" : Nom} })
 def Set_Event_date_debut(id,Date_debut): 
-    return db.event.update_One({"_id" : id} ,{"$set" :  {"date_debut" : Date_debut} })
+    return db.event.update_one({"_id" : id} ,{"$set" :  {"date_debut" : Date_debut} })
 def Set_Event_date_fin(id,Date_fin): 
-    return db.event.update_One({"_id" : id} ,{"$set" :  {"date_fin" : Date_fin} })
+    return db.event.update_one({"_id" : id} ,{"$set" :  {"date_fin" : Date_fin} })
 def Set_Event_places_max(id,Places_max): 
-    return db.event.update_One({"_id" : id} , {"$set" : {"places_max" : Places_max} })
+    return db.event.update_one({"_id" : id} , {"$set" : {"places_max" : Places_max} })
 def Set_Event_places_min(id,Places_libres): 
-    return db.event.update_One({"_id" : id} ,{"$set" :  {"place_libres": Places_libres} })
+    return db.event.update_one({"_id" : id} ,{"$set" :  {"place_libres": Places_libres} })
 def Set_Event_cash_price(id,Cash_price): 
-    return db.event.update_One({"_id" : id}, {"$set" :  {"cash_price" : Cash_price} })
+    return db.event.update_one({"_id" : id}, {"$set" :  {"cash_price" : Cash_price} })
 def Set_Event_Status(id,Status): 
-    return db.event.update_One({"_id" : id}, {"$set" :  {"status" : Status} })
+    return db.event.update_one({"_id" : id}, {"$set" :  {"status" : Status} })
 
 # match
 def Add_Match(Equipe1,Equipe2,Date,Score1,Score2,Wineur, Arbitre, _event):
-    return db.Event.insertone({"equipe1" : Equipe1, "equipe2" : Equipe2, "date" : Date, "score1" : Score1, "score2" : Score2, "wineur" : Wineur, "arbitre" : Arbitre,"_event" : _event})
+    return db.Event.insert_one({"equipe1" : Equipe1, "equipe2" : Equipe2, "date" : Date, "score1" : Score1, "score2" : Score2, "wineur" : Wineur, "arbitre" : Arbitre,"_event" : _event})
 
 
 def Sup_Match(id):
@@ -205,22 +205,22 @@ def Get_Match_event(id):
 
 #setteur Match
 def Set_Match(id,Equipe1,Equipe2,Date,Score1,Score2,Wineur, Arbitre, _event ):
-    return db.match.update_One({"_id" : id} ,{"$set" :  {"equipe1" : Equipe1, "equipe2" : Equipe2, "date" : Date, "score1" : Score1, "score2" : Score2, "wineur" : Wineur, "arbitre" : Arbitre ,"_event": _event} })
+    return db.match.update_one({"_id" : id} ,{"$set" :  {"equipe1" : Equipe1, "equipe2" : Equipe2, "date" : Date, "score1" : Score1, "score2" : Score2, "wineur" : Wineur, "arbitre" : Arbitre ,"_event": _event} })
 
 def Set_Match_equipe1(id,Equipe): 
-    return db.match.update_One({"_id" : id} ,{"$set" :  {"equipe1" : Equipe} })
+    return db.match.update_one({"_id" : id} ,{"$set" :  {"equipe1" : Equipe} })
 def Set_Match_equipe2(id,Equipe): 
-    return db.match.update_One({"_id" : id} ,{"$set" :  {"equipe2" : Equipe} })
+    return db.match.update_one({"_id" : id} ,{"$set" :  {"equipe2" : Equipe} })
 def Set_Match_date(id,Date): 
-    return db.match.update_One({"_id" : id} ,{"$set" :  {"date" : Date} })
+    return db.match.update_one({"_id" : id} ,{"$set" :  {"date" : Date} })
 def Set_Match_score1(id,Score): 
-    return db.match.update_One({"_id" : id} , {"$set" : {"score1": Score} })
+    return db.match.update_one({"_id" : id} , {"$set" : {"score1": Score} })
 def Set_Match_score2(id,Score): 
-    return db.match.update_One({"_id" : id} ,{"$set" :  {"score2": Score} })
+    return db.match.update_one({"_id" : id} ,{"$set" :  {"score2": Score} })
 def Set_Match_wineur(id,Wineur): 
-    return db.match.update_One({"_id" : id}, {"$set" :  {"wineur" : Wineur} })
+    return db.match.update_one({"_id" : id}, {"$set" :  {"wineur" : Wineur} })
 def Set_Match_wineur(id,Arbitre): 
-    return db.match.update_One({"_id" : id}, {"$set" :  {"arbitre" : Arbitre} })
+    return db.match.update_one({"_id" : id}, {"$set" :  {"arbitre" : Arbitre} })
 def Set_Match_wineur(id,_event): 
-    return db.match.update_One({"_id" : id}, {"$set" :  {"_event" : _event} })
+    return db.match.update_one({"_id" : id}, {"$set" :  {"_event" : _event} })
 
