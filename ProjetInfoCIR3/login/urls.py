4be87673dec1cf8a366a89730519a2d8a1a5fca4
KhalidBensaber <khalid.bensaber@student.junia.com>
2025-01-17 07:25:00 +0100

    Loggin + signup withou CSRF

--- ProjetInfoCIR3/login/urls.py
+++ ProjetInfoCIR3/login/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     #path('', views.login, name='login'),
-    path('login/', views.login, name='login_api'),  # Endpoint pour login
+    path('', views.login, name='login_api'),  # Endpoint pour login
     path('logout/', views.logout, name='logout_api'),  # Endpoint pour logout
     path('check-auth/', views.check_auth, name='check_auth_api'),  # Vérification de l'authentification
     
